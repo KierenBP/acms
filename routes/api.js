785339605871cd3764fabb6e89b35d13a8356d1c
Kieren BP <kieren@kieren.xyz>
2017-03-21 10:30:17 +1300

    Move bing background route into api without auth required

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const auth = require('./../helpers/auth');
 const tools = require('./../helpers/tools');
 const core = require('./../helpers/core');
@@ -7,6 +8,13 @@ const core = require('./../helpers/core');
 const clients = require('./clients');
 
 const router = express.Router();
+
+// Bing Background
+// No Auth required
+router.use('/bingbg', (req, res) => {
+  const newurl = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US';
+  request(newurl).pipe(res);
+});
 
 // Auth check
 // This must be before any router requests as they will NOT be authenticated otherwise
@@ -22,6 +30,7 @@ router.get('/', (req, res) => {
     user: res.locals.user,
   });
 });
+
 
 router.use('/clients', clients);
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,7 @@
 const express = require('express');
-const request = require('request');
 
 const router = express.Router();
 
-router.use('/bingbg', (req, res) => {
-  const newurl = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US';
-  request(newurl).pipe(res);
-});
 router.get('/', (req, res) => {
   res.send('Hello');
 });
