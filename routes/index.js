bd9a595d468d88b4bab548ee2731860bcb6f4634
Kieren BP <kieren@kieren.xyz>
2017-05-01 10:40:05 +1200

    Justifying react handling

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ const path = require('path');
 
 const router = express.Router();
 
+// Route for react files to be send to the client for CLIENT SIDE rendering
 router.use(express.static(path.resolve('./views')));
 
+// Hand over control to React Router
 router.get(['/', '/#*'], (req, res) => {
-  // res.send('Hello');
   res.sendFile(path.resolve('./views/index.html'));
 });
 
